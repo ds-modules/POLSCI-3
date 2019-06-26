d5efe0663e3141972995aa7fd6a24a841cd8f519
wmceachen <wmceachen@berkeley.edu>
2019-06-25 23:13:24 -0400

    added some president csv

--- beta materials/clean_data.py
+++ beta materials/clean_data.py
@@ -1,5 +1,13 @@
 import pandas as pd
-elections = pd.read_csv('1976-2016-president.csv', error_bad_lines=False, low_memory=False)
-sorted_elections = elections.sort_values('candidatevotes', ascending=False).groupby(['year', 'state',])['candidate'].first().unstack().transpose()
+import numpy as np
+elections = pd.read_csv('1976-2016-president.csv',
+                        error_bad_lines=False, low_memory=False)
+sorted_elections = elections.sort_values('candidatevotes', ascending=False).groupby(
+    ['year', 'state', ])['candidate'].first().unstack().transpose()
 sorted_elections.columns = sorted_elections.columns.map(str)
-print(sorted_elections.apply(lambda col: col.mode()).transpose())
+data = sorted_elections.apply(lambda col: col.mode()).transpose()
+
+df = pd.DataFrame(data=[np.array(data.index), np.array(data.values).flatten()]).T
+df.columns = ['year', 'president']
+print(df)
+df.to_csv('election_outcomes.csv')
